395cedf7bcff2df8d5a773d7f736509fac09435e
Laura <laurahenrique9@gmail.com>
2024-04-17 03:13:14 -0300

    Alterações em update

--- public/app/controllers/ProductController.php
+++ public/app/controllers/ProductController.php
@@ -1,10 +1,8 @@
 <?php
 require_once 'app/models/Product.php';
 
-
 class ProductController {
     private $pdo;
-
 
     public function __construct(PDO $pdo) {
         $this->pdo = $pdo;
@@ -20,19 +18,14 @@ class ProductController {
         return $productModel->all();
     }
 
-
     public function delete($productId) {
         $productModel = new Product($this->pdo);
         $productModel->delete($productId);
     }
+
     public function update($productId, $nome, $descricao, $preco, $quantidade, $imagem) {
         $productModel = new Product($this->pdo);
         return $productModel->update($productId, $nome, $descricao, $preco, $quantidade, $imagem);
-    }
-    public function getProductDetails($productId) {
-        $stmt = $this->pdo->prepare("SELECT * FROM produtos WHERE id = ?");
-        $stmt->execute([$productId]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
 ?>
