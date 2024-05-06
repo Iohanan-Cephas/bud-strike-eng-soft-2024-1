88341a219957fc8b6b212c81ee6e256bc4c64db3
messiribeiro <micaelribeirodossantos@gmail.com>
2024-05-06 11:49:41 -0300

    addLoginAndRegisterPages

--- public/update.php
+++ public/update.php
@@ -60,7 +60,7 @@ try {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="update-styles.css">
+    <link rel="stylesheet" href="./styles/update-styles.css">
     
 </head>
 <body>
