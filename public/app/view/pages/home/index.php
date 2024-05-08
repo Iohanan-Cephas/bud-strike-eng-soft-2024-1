ff16d957e7ea012ca40ba00953b18e824cd29f97
messiribeiro <micaelribeirodossantos@gmail.com>
2024-05-08 01:51:10 -0300

    implementei a funcionalidade de registro

---
+++ public/app/view/pages/home/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+        rel="stylesheet">
+</head>
+
+<title>BudStrike</title>
+</head>
+
+<body>
+    <header>
+        <div id="options">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+            <h1>BS</h1>
+            <a href="../cart/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg></a>
+
+        </div>
+        <img src="../../assets/banner.webp" alt="">
+    </header>
+    <main>
+        <h2>Mais vendidos</h2>
+        <div id="products">
+            <div id="product">
+                <div id="img-and-title">
+                    <img src="../../assets/mouse-multilaser.png" alt="">
+                    <p>Mouse gamer última geração</p>
+                </div>
+                <div id="stars-and-price">
+                    <div id="stars">
+                        <img src="../../assets/star.svg" alt="">
+                        <p>5.0</p>
+                    </div>
+                    <p id="price" >R$ 239,90</p>
+                </div>
+            </div>
+            <div id="product">
+                <div id="img-and-title">
+                    <img src="../../assets/mouse-multilaser.png" alt="">
+                    <p>Mouse gamer última geração</p>
+                </div>
+                <div id="stars-and-price">
+                    <div id="stars">
+                        <img src="../../assets/star.svg" alt="">
+                        <p>5.0</p>
+                    </div>
+                    <p id="price" >R$ 239,90</p>
+                </div>
+            </div>
+            <div id="product">
+                <div id="img-and-title">
+                    <img src="../../assets/mouse-multilaser.png" alt="">
+                    <p>Mouse gamer última geração</p>
+                </div>
+                <div id="stars-and-price">
+                    <div id="stars">
+                        <img src="../../assets/star.svg" alt="">
+                        <p>5.0</p>
+                    </div>
+                    <p id="price" >R$ 239,90</p>
+                </div>
+            </div>
+            <div id="product">
+                <div id="img-and-title">
+                    <img src="../../assets/mouse-multilaser.png" alt="">
+                    <p>Mouse gamer última geração</p>
+                </div>
+                <div id="stars-and-price">
+                    <div id="stars">
+                        <img src="../../assets/star.svg" alt="">
+                        <p>5.0</p>
+                    </div>
+                    <p id="price" >R$ 239,90</p>
+                </div>
+            </div>
+            
+            
+            
+            </div>
+        </div>
+    </main>
+    <footer>
+        BudStrike &copy; 2024
+    </footer>
+
+    <script src="./script.js"></script>
+</body>
+
+</html>
--- public/app/view/pages/register/index.php
+++ public/app/view/pages/register/index.php
@@ -1,3 +1,43 @@
+<?php
+// Inclua o arquivo de conexão com o banco de dados
+include('../../../config/config.php');
+
+// Verifique se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verifique se todos os campos obrigatórios foram preenchidos
+    if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password_confirm']) && isset($_POST['terms'])) {
+        
+        // Obtenha os valores do formulário
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $password_confirm = $_POST['password_confirm'];
+        
+        // Verifique se as senhas coincidem
+        if ($password !== $password_confirm) {
+            echo "As senhas não coincidem. Por favor, tente novamente.";
+        } else {
+            // Preparar e executar a query SQL para inserir dados no banco de dados
+            $sql = "INSERT INTO usuarios (username, password) VALUES (:username, :password)";
+            $stmt = $pdo->prepare($sql);
+
+            // Bind dos parâmetros
+            $stmt->bindParam(':username', $username);
+            $stmt->bindParam(':password', $password);
+
+            // Executa a query
+            if ($stmt->execute()) {
+                header("Location: ../home");
+                exit;
+            } else {
+                //faço nada
+            }
+        }
+    } else {
+        //deixa q o javascript trabaia nessa parte
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,44 +45,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BudStrike</title>
     <link rel="stylesheet" href="styles.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
         Cadastre-se
     </header>
     <main>
-        <div class="login-container" >
-            <form class="login-form" action="">
-                <div class="user-container" >
+        <div class="login-container">
+            <form class="login-form" method="POST" action="">
+                <div class="user-container">
                     <img src="../../assets/user.svg" alt="">
-                    <input id="user-input" placeholder="usuário" type="text">
+                    <input id="user-input" name="username" placeholder="usuário" type="text" required>
                 </div>
                 <div class="user-password" id="password">
                     <img src="../../assets/lock.svg" alt="">
-                    <input type="password" class="password" id="password-input" placeholder="senha" type="text">
+                    <input type="password" class="password" name="password" id="password-input" placeholder="senha" required>
                 </div>
                 <div class="user-password" id="password-confirm">
-                  <img src="../../assets/lock.svg" alt="">
-                  <input type="password" class="password" id="password-input-confirm" placeholder="confirmar senha" type="text">
-              </div>
-              <div id="terms" >
-                <input type="checkbox" name="terms" id="">
-                <p>concordo com todos os <a href="../terms/index.html">termos</a></p>
-              </div>
-                <div class="login-button" >
-                    <button type="submit" >Cadastrar</button>
+                    <img src="../../assets/lock.svg" alt="">
+                    <input type="password" class="password" id="password-input-confirm" name="password_confirm" placeholder="confirmar senha" required>
+                </div>
+                <div id="terms">
+                    <input type="checkbox" name="terms" id="terms" required>
+                    <p>concordo com todos os <a href="../terms/index.html">termos</a></p>
+                </div>
+                <div class="login-button">
+                    <button type="submit">Cadastrar</button>
                 </div>
             </form>
-
         </div>
     </main>
     <footer>
         BudStrike &copy; 2024
     </footer>
-
-    <script src="./script.js"></script>
 </body>
 </html>
