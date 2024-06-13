5f4f78fd90a7139c2463581c80f1613c7921e1d0
messiribeiro <micaelribeirodossantos@gmail.com>
2024-06-13 02:28:57 -0300

    implementei o menu hamburguer da página home

--- public/app/view/pages/home/script.js
+++ public/app/view/pages/home/script.js
@@ -1,5 +1,22 @@
 
 const logoutButton = document.querySelector('#logout');
+const menuButton = document.querySelector('#menuButton');
+const closeSide = document.querySelector("#close-side")
+const menu = document.querySelector('#menu');
+
+
+closeSide.addEventListener('click', () => {
+    menu.classList.add('menu-closed');
+    menu.classList.remove('menu-opened');
+
+
+})
+
+menuButton.addEventListener('click', () => {
+    menu.classList.remove('menu-closed');
+    menu.classList.add('menu-opened');
+})
+
 
 
 console.log("teste" + logoutButton);
